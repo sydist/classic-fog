576a99c6082765cf63843cf482e9a3a66b85169e
sydist <81040138+sydist@users.noreply.github.com>
2024-07-30 12:27:05 +0200

    Update to 1.21

--- assets/minecraft/shaders/include/fog.glsl
+++ assets/minecraft/shaders/include/fog.glsl
@@ -1,19 +1,17 @@
 #version 150
 
 vec4 linear_fog(vec4 inColor, float vertexDistance, float fogStart, float fogEnd, vec4 fogColor) {
-    fogStart *= 0.3333333;
+    fogStart /= 3;
     
     if (vertexDistance <= fogStart) {
         return inColor;
     }
 
-    float fogValue = vertexDistance <= fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
+    float fogValue = vertexDistance < fogEnd ? smoothstep(fogStart, fogEnd, vertexDistance) : 1.0;
     return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
 }
 
 float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
-    fogStart *= 0.3333333;
-    
     if (vertexDistance <= fogStart) {
         return 1.0;
     } else if (vertexDistance >= fogEnd) {
@@ -23,6 +21,12 @@ float linear_fog_fade(float vertexDistance, float fogStart, float fogEnd) {
     return smoothstep(fogEnd, fogStart, vertexDistance);
 }
 
-float fog_distance(mat4 modelViewMat, vec3 pos, int shape) {
-    return length((modelViewMat * vec4(pos, 1.0)).xyz);
+float fog_distance(vec3 pos, int shape) {
+    if (shape == 0) {
+        return length(pos);
+    } else {
+        float distXZ = length(pos.xz);
+        float distY = abs(pos.y);
+        return max(distXZ, distY);
+    }
 }
